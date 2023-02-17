0ab6d93bd59b7275c85f8a07a06b28779edfd340
John-Esele <st.johnehiz@gmail.com>
2023-02-17 15:18:16 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 /**
  * main - A program that prints the size of various computer types
- * Return 0 (Success)
+ * Return: 0 (Success)
 */
 
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float f;
+char a;
+int b;
+long int c;
+long long int d;
+float f;
 
 printf("Size of a char: %du byte(s)\n", sizeof(char));
 printf("Size of a ink: %du byte(s)\n", sizeof(int));
