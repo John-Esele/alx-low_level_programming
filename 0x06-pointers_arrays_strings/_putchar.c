01f39e0cec774d6aa833783247d2551c6c8bd8eb
John-Esele <johnehiz@gmail.com>
2023-07-04 05:40:10 +0000

    C - Even more pointers, arrays and strings

--- 0x06-pointers_arrays_strings/_putchar.c
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -1,14 +1,12 @@
 #include <unistd.h>
-  2 /**
-  3  * _putchar Write the character c to stdout
-  4  * @n: The character to print
-  5  *
-  6  * Return: On success 1.
-  7  * On error, -1 is returned, and errno is set appproppriately.
-  8  */
-  9 int _putchar(char c)
- 10 {
- 11         return (write(1, &c, 1));
- 12 }
-
-
+/**
+ * _putchar Write the character c to stdout
+ * @n: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appproppriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
